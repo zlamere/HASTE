1e10941e7dfcaf8549140f24c4a41a8944cbdcf1
HATSwg <whitman.dailey@nnsa.doe.gov>
2018-12-11 23:14:19 -0500

    Add GNU GPL v3 license header to each source file.

--- source/common/Random_Numbers.f90
+++ source/common/Random_Numbers.f90
@@ -1,3 +1,19 @@
+!-------------------------------------------------------------------------------
+!   Copyright (C) 2017  Whitman T. Dailey
+!   
+!   This program is free software: you can redistribute it and/or modify
+!   it under the terms of the GNU General Public License version 3 as 
+!   published by the Free Software Foundation.
+!   
+!   This program is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU General Public License for more details.
+!   
+!   You should have received a copy of the GNU General Public License
+!   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!-------------------------------------------------------------------------------
+
 # if IMKL
     Include 'mkl_vsl.f90'  !Include allows use of Intel MKL random number generators
     !The Intel MKL is only portable to IA32 and IA64 -like architectures...
