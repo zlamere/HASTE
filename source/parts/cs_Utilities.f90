5831fe85ec1347da052e1bf6602382f84213ff62
HATSwg <39804032+HATSwg@users.noreply.github.com>
2019-06-05 10:37:04 -0400

    Switches indexing order for tabular PDF.

--- source/parts/cs_Utilities.f90
+++ source/parts/cs_Utilities.f90
@@ -29,17 +29,15 @@ Function Tabular_Cosine_pdf(mu0cm,n1,ua1,n2,ua2,Econv) Result(p)
     Real(dp) :: p
     Real(dp), Intent(In) :: mu0cm
     Integer, Intent(In) :: n1,n2
-    Real(dp), Intent(In) :: ua1(1:n1,1:2),ua2(1:n2,1:2)
+    Real(dp), Intent(In) :: ua1(1:2,1:n1),ua2(1:2,1:n2)
     Real(dp), Intent(In) :: Econv
     Integer :: i
     Real(dp) :: p1,p2
 
-    i = Bisection_Search(mu0cm,ua1(:,1),n1)
-    ! p1 = Exp(Linear_Interp(mu0cm,ua1(i-1,1),ua1(i,1),Log(ua1(i-1,2)),Log(ua1(i,2))))
-    p1 = Exp(Linear_Interp(mu0cm,ua1(i-1,1),ua1(i,1),ua1(i-1,2),ua1(i,2)))
-    i = Bisection_Search(mu0cm,ua2(:,1),n2)
-    ! p2 = Exp(Linear_Interp(mu0cm,ua2(i-1,1),ua2(i,1),Log(ua2(i-1,2)),Log(ua2(i,2))))
-    p2 = Exp(Linear_Interp(mu0cm,ua2(i-1,1),ua2(i,1),ua2(i-1,2),ua2(i,2)))
+    i = Bisection_Search(mu0cm,ua1(1,:),n1)
+    p1 = Exp(Linear_Interp(mu0cm,ua1(1,i-1),ua1(1,i),ua1(2,i-1),ua1(2,i)))
+    i = Bisection_Search(mu0cm,ua2(1,:),n2)
+    p2 = Exp(Linear_Interp(mu0cm,ua2(1,i-1),ua2(1,i),ua2(2,i-1),ua2(2,i)))
     p = p1 + Econv * (p2 - p1)
 End Function Tabular_Cosine_pdf
 
